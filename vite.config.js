c6c81902f5141fa45c1c304a09d36f9de229891f
dppablito4-oss <dppablito4-oss@users.noreply.github.com>
2026-04-17 18:09:24 -0500

    Test commit from Antigravity

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// Prueba de commit y push desde Antigravity
 export default defineConfig({
   plugins: [react()],
   base: '/',
